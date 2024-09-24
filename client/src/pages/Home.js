b67fe7c1f0facc924129fc1680d8213a113f05be
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2024-09-25 00:15:56 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,23 +2,9 @@ const Home = function () {
 
     return (
     <div class="flex flex-col justify-center items-center font-display">
-      <div>
-        <h1 class="text-4xl font-bold pt-4 my-4">
-          Boost your productivity.<br/>
-          Start using our app today.
-        </h1>
-      </div>
-      <div>
-        <p class="text-xl m-4">
-          Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.
+        <p>
+         /signup & /signin
         </p>
-      </div>
-      <div>
-        <div class="flex gap-4 items-center">
-        <button class="bg-indigo-600 p-2 rounded-md text-white font-semibold">Get Started</button>
-        <a href="#" class="font-semibold">Learn More →</a>
-      </div>
-      </div>
     </div>
   
     );
